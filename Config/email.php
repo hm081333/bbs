1487651792a9a6879992ee862a86cbfbd8f71f15
hm081333 <522751485@qq.com>
2017-06-18 18:13:59 +0800

    准备把所有静态页套上js框架。vue.js...

---
+++ Config/email.php
@@ -0,0 +1,12 @@
+<?php   
+return array (
+  'email' => 
+  array (
+    'host' => 'smtp.gmail.com',
+    'username' => 'lyihe2@gmail.com',
+    'password' => 'asyatluiwtuwgzuc',
+    'from' => 'lyihe2@gmail.com',
+    'fromName' => 'LYi-Ho',
+    'sign' => '来自LYi-Ho，请勿回复~',
+  ),
+);
---
+++ Config/sms.php
@@ -0,0 +1,8 @@
+<?php   
+return array (
+  'smsbao' => 
+  array (
+    'username' => 'hm081333',
+    'password' => 'asd110',
+  ),
+);
