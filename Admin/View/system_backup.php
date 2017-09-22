2b02283c21fdeb02e8b1357d6eb4097b0826f540
LYi-Ho <522751485@qq.com>
2017-09-23 02:28:38 +0800

    备份数据库功能doing

---
+++ Admin/View/system_backup.php
@@ -0,0 +1,57 @@
+<h3 class="center"><?php echo T('系统备份还原') ?></h3>
+<fieldset>
+	<legend><?php echo T('系统备份还原') ?></legend>
+	<div class="row">
+		<div class="col s12 center">
+			<ul class="collapsible" data-collapsible="accordion">
+				<?php foreach ($files as $key => $file): ?>
+					<?php if (is_array($file)): ?>
+						<li>
+							<div class="collapsible-header">
+								<?php echo $key; ?>
+							</div>
+							<div class="collapsible-body" style="padding: 0;">
+								<?php foreach ($file as $min): ?>
+									<a style="display: block; padding: 0.6rem;">
+										<?php echo $min; ?>
+									</a>
+								<?php endforeach; ?>
+							</div>
+						</li>
+					<?php else: ?>
+						<li style="height: 3rem; line-height: 3rem; padding: 0 1rem;">
+							<a>
+								<?php echo $file; ?>
+							</a>
+						</li>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			</ul>
+		</div>
+		<div class="col s12 center">
+			<button class="btn waves-effect waves-light modal-trigger" data-target="BackupModal">
+				<?php echo T('备份'); ?>
+			</button>
+		</div>
+</fieldset>
+<!-- 备份数据库弹窗 -->
+<div id="BackupModal" class="modal">
+	<div class="modal-content center">
+		<h4>备份数据库</h4>
+		<p>备份说明</p>
+		<form id="Backup" method="post" onsubmit="return false;">
+			<input type="hidden" name="service" value="System.backup">
+			<input type="hidden" name="action" value="post">
+			<div class="col s12">
+				<div class="input-field">
+					<i class="material-icons prefix">vpn_key</i>
+					<input id="password" name="password" type="password">
+					<label for="password"><?php echo T('请输入密码') ?></label>
+				</div>
+			</div>
+			<button type="submit" class="btn waves-effect waves-light"><?php echo T('确定'); ?></button>
+			<button onclick="$('#BackupModal').modal('close')"
+					class="btn waves-effect waves-light"><?php echo T('取消'); ?></button>
+		</form>
+	</div>
+</div>
