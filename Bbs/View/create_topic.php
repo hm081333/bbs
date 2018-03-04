fc78e077ad3e5c4cd640a8c07ab5b4a41227d297
LYi-Ho <522751485@qq.com>
2018-03-04 17:41:20 +0800

    修改发帖页面

--- Bbs/View/create_topic.php
+++ Bbs/View/create_topic.php
@@ -1,3 +1,9 @@
+<style>
+	#detail {
+		width: 100%;
+		height: 500px;
+	}
+</style>
 <fieldset>
 	<legend><?php echo T('新帖子'); ?></legend>
 	<div class="row">
@@ -15,10 +21,10 @@
 					<input name="topic" type="text" id="topic" class="validate" length="100">
 					<label for="topic"><?php echo T('标题'); ?></label>
 				</div>
-				<div class="input-field col s12">
+				<!--<div class="input-field col s12">
 					<textarea name="detail" id="detail" class="materialize-textarea validate" length="1000"></textarea>
-					<label for="detail"><?php echo T('正文内容'); ?></label>
-				</div>
+					<label for="detail"><?php /*echo T('正文内容'); */ ?></label>
+				</div>-->
 				<div class="input-field col s12">
 					<select name="class_id">
 						<option disabled selected><?php echo T('请选择'); ?></option>
@@ -57,7 +63,9 @@
 					<br/>
 				<?php endif; ?>
 			</div>
-			<div id="contant" class="col s12">Test 2</div>
+			<div id="contant" class="col s12">
+				<script id="detail" type="text/plain"> 这里写你的初始化内容</script>
+			</div>
 			<div class="col s12 center">
 				<button type="submit" name="submit"
 				        class="btn waves-effect waves-light"><?php echo T('立即发布'); ?></button>
@@ -66,3 +74,24 @@
 		</form>
 	</div>
 </fieldset>
+
+<script>
+	window.NEDITOR_UPLOAD = '<?php echo NOW_WEB_SITE ?>';
+	window.UEDITOR_HOME_URL = "/Public/static/js/neditor/";
+	//window.UEDITOR_HOME_URL = '<?php //echo URL; ?>//'
+</script>
+
+<script type="text/javascript" charset="utf-8"
+        src="<?php echo DI()->tool->staticPath('js/neditor/neditor.config.js'); ?>"></script>
+<script type="text/javascript" charset="utf-8"
+        src="<?php echo DI()->tool->staticPath('js/neditor/neditor.all.js'); ?>"></script>
+<!--建议手动加在语言，避免在ie下有时因为加载语言失败导致编辑器加载失败-->
+<!--这里加载的语言文件会覆盖你在配置项目里添加的语言类型，比如你在配置项目里配置的是英文，这里加载的中文，那最后就是中文-->
+<script type="text/javascript" charset="utf-8"
+        src="<?php echo DI()->tool->staticPath('js/neditor/i18n/zh-cn/zh-cn.js'); ?>"></script>
+<!-- 实例化编辑器 -->
+<script type="text/javascript">
+	//实例化编辑器
+	//建议使用工厂方法getEditor创建和引用编辑器实例，如果在某个闭包下引用该编辑器，直接调用UE.getEditor('editor')就能拿到相关的实例
+	var ue = UE.getEditor('detail');
+</script>
