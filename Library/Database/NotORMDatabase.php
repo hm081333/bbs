588530fdc1cfed0f79ff3f167a850ccd0767ca8f
LYi-Ho <522751485@qq.com>
2020-03-27 23:28:45 +0800

    使用命令行运行代码时，PDO使用长连接，减少重复连接数据库

--- Library/Database/NotORMDatabase.php
+++ Library/Database/NotORMDatabase.php
@@ -29,6 +29,10 @@ class NotORMDatabase extends \PhalApi\Database\NotORMDatabase
     {
         $dsn = sprintf('mysql:dbname=%s;host=%s;port=%d', $dbCfg['name'], $dbCfg['host'] ?? 'localhost', $dbCfg['port'] ?? 3306);
         $charset = $dbCfg['charset'] ?? 'UTF8';
+        $option = $dbCfg['option'] ?? [];
+        if (IS_CLI) {
+            $option[PDO::ATTR_PERSISTENT] = true;
+        }
 
         // 支持sql server
         if (!empty($dbCfg['type']) && strtolower($dbCfg['type']) == 'sqlserver') {
@@ -39,7 +43,7 @@ class NotORMDatabase extends \PhalApi\Database\NotORMDatabase
             $dsn,
             $dbCfg['user'],
             $dbCfg['password'],
-            $dbCfg['option'] ?? []
+            $option
         );
         $pdo->exec("SET NAMES '{$charset}'");
 
