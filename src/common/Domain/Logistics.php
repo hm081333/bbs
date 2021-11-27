990347c9690ddaa76d5d36adc25a14038fac48f0
LYi-Ho <522751485@qq.com>
2021-11-27 10:38:09 +0800

    修复物流接口

--- src/common/Domain/Logistics.php
+++ src/common/Domain/Logistics.php
@@ -29,7 +29,14 @@ class Logistics
      */
     public static function queryLogistics(string $code, string $sn)
     {
-        $result = self::DI()->curl->get('http://www.kuaidi100.com/query?type=' . $code . '&postid=' . $sn);
-        return json_decode($result, true);
+        // $result = self::DI()->curl->get('http://www.kuaidi100.com/query?type=' . $code . '&postid=' . $sn);
+        // return json_decode($result, true);
+        return self::DI()->curl->setCookie([
+            'Hm_lpvt_22ea01af58ba2be0fec7c11b25e88e6c' => time(),
+        ])->setHeader([
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Referer' => 'http://baidu.kuaidi100.com/',
+            'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+        ])->json_get("http://baidu.kuaidi100.com/query?type={$code}&postid={$sn}&id=4&temp=0." . rand(100000000000000, 999999999999999));
     }
 }
