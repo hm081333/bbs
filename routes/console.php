9b74b03e6b779fda682ea973fe6c4ab077f8b0d2
LYi-Ho <522751485@qq.com>
2023-09-05 00:17:17 +0800

    修改数据类型，时间保存整数时间戳

--- routes/console.php
+++ routes/console.php
@@ -19,6 +19,13 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('testa', function () {
+    $res = \App\Utils\Tools::curl(5)
+        ->setHeader([
+            'Referer' => 'https://fund.eastmoney.com/data/fundranking.html',
+            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        ])
+        ->get("https://fund.eastmoney.com/data/rankhandler.aspx?op=ph&dt=kf&ft=lof&rs=&gs=0&sc=dm&st=asc&sd=2023-09-05&ed=2023-09-05&pi=1&pn=200&dx=0");
+    dd($res);
     $url = "https://fund.eastmoney.com/pingzhongdata/320007.js";
     $res = \App\Utils\Tools::curl(5)->setHeader([
         'User-Agent' => 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1',
