99f2a8249d4a4cf166c10b0bf6d367fbce365387
LYi-Ho <522751485@qq.com>
2023-09-10 18:25:55 +0800

    增加 同步（天天基金网）历史净值

--- database/migrations/2023_08_16_132102_create_funds_table.php
+++ database/migrations/2023_08_16_132102_create_funds_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('code')->index()->comment('基金代码');
             $table->string('name')->comment('基金名称');
             $table->string('pinyin_initial')->comment('基金名称拼音首字母');
-            $table->string('type')->comment('基金类型');
+            $table->string('type')->nullable()->comment('基金类型');
             $table->unsignedInteger('created_at')->nullable()->comment('创建时间');
             $table->unsignedInteger('updated_at')->nullable()->comment('更新时间');
             $table->unsignedInteger('deleted_at')->nullable()->comment('删除时间');
