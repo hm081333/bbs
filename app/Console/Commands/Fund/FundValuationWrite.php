c07cb1ba76e9a55cff5bf1a0d5f906dbbe679050
LYi-Ho <522751485@qq.com>
2023-11-03 15:06:23 +0800

    优化以及修复BUG

--- app/Console/Commands/Fund/FundValuationWrite.php
+++ app/Console/Commands/Fund/FundValuationWrite.php
@@ -40,7 +40,7 @@ class FundValuationWrite extends Command
     /**
      * @var float|int 超时时间，避免定时器一直生成新进程运行，老进程不退出。单位秒。
      */
-    private $timeout = 10 * 60;
+    private $timeout = 5 * 60;
 
     /**
      * @var string Redis集合键名
@@ -112,20 +112,17 @@ class FundValuationWrite extends Command
         } else {
             $this->comment('写入基金估值');
             if (Redis::exists($this->redis_key)) {
-                while (true) {
+                while (!empty($inserts = Redis::spop($this->redis_key, $this->once_write_count))) {
                     $start_time = microtime(true);
-                    $inserts = Redis::spop($this->redis_key, $this->once_write_count);
                     $this->info('成功|集合中获取|' . count($inserts) . '|条基金估值|耗时：' . (microtime(true) - $start_time) . ' 秒');
                     if (empty($inserts)) break;
                     $start_time = microtime(true);
                     try {
-                        FundValuation::insert(array_map(function ($value) {
-                            return Tools::jsonDecode($value);
-                        }, $inserts));
+                        FundValuation::insert(array_map(fn($value) => Tools::jsonDecode($value), $inserts));
                         $this->info('成功|写入|' . count($inserts) . '|条基金估值|耗时：' . (microtime(true) - $start_time) . ' 秒');
                     } catch (\Exception $e) {
                         $this->info('失败|写入|' . count($inserts) . '|条基金估值|耗时：' . (microtime(true) - $start_time) . ' 秒');
-                        $this->error($e->getMessage());
+                        // $this->error($e->getMessage());
                         Redis::sadd($this->redis_key, ...$inserts);
                     }
                     if ((microtime(true) - $global_start_time) >= $this->timeout) {
