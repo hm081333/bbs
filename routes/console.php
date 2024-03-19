9da26a5a7f546a2aaae9b08b41511cd2115359d6
OHO <522751485@qq.com>
2024-03-19 21:12:32 +0800

    修改基金表名

--- routes/console.php
+++ routes/console.php
@@ -29,4 +29,26 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('testa', function () {
+    $global_start_time = microtime(true);
+    while (true) {
+        $start_time = microtime(true);
+        $failed_jobs = \Illuminate\Support\Facades\DB::table('failed_jobs_bak')->limit(500)->orderBy('id')->get()->map(fn($item) => (array)$item);
+        $this->info('完成|查询|耗时：' . Tools::secondToTimeText(microtime(true) - $start_time));
+        $this->info('select-' . $failed_jobs->count());
+        if ($failed_jobs->count() <= 0) break;
+        $res = \Illuminate\Support\Facades\DB::table('failed_jobs')->insert($failed_jobs->toArray());
+        $this->info('完成|插入|耗时：' . Tools::secondToTimeText(microtime(true) - $start_time));
+        $this->info('insert-' . $res);
+        $exitCode = Artisan::call('queue:retry all');
+        $this->info('完成|重试|耗时：' . Tools::secondToTimeText(microtime(true) - $start_time));
+        $this->info('commandExitCode-' . $exitCode);
+        if ($res && $exitCode === 0) {
+            $res = \Illuminate\Support\Facades\DB::table('failed_jobs_bak')->whereIn('id', $failed_jobs->pluck('id'))->delete();
+            $this->info('完成|删除|耗时：' . Tools::secondToTimeText(microtime(true) - $start_time));
+            $this->info('delete-' . $res);
+        }
+    }
+    $this->info('结束|耗时：' . Tools::secondToTimeText(microtime(true) - $global_start_time));
+
+    // $this->call('queue:retry all');
 })->purpose('cesi');
